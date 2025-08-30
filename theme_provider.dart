e02ae9fa956dc71c6aec7cb027c33b9d3ac30b4d
kinzarasheed-coder <kinzarasheed332@gmail.com>
2025-08-30 12:36:32 -0700

    Add files via upload

---
+++ theme_provider.dart
@@ -0,0 +1,25 @@
+import 'package:final_year_project/themes/light_mode.dart';
+import 'package:flutter/material.dart';
+
+import 'dark_mode.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+  bool get isDarkMode => _themeData == darkMode;
+
+  set themeData (ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+  void toggleTheme(){
+    if(_themeData == lightMode){
+      themeData = darkMode;
+    }
+    else
+      {
+        themeData = lightMode;
+      }
+  }
+}
