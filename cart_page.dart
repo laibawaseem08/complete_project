a4c8583eb7e02bc40adf265a44dd6b1966d85955
Sarahassan14 <148893698+Sarahassan14@users.noreply.github.com>
2025-08-31 01:00:39 +0500

    Add files via upload

---
+++ cart_page.dart
@@ -0,0 +1,105 @@
+import 'package:final_year_project/components/my_button.dart';
+import 'package:final_year_project/components/my_cart_tile.dart';
+import 'package:final_year_project/pages/payment_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/restaurant.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Restaurant>(builder: (context, restaurant,child) {
+      // cart
+      final userCart = restaurant.cart;
+
+      // scaffold UI
+      return Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+                "Cart",
+            ),
+          ),
+          backgroundColor: Colors.transparent,
+          foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+          actions: [
+            // clear cart button
+            IconButton(
+                onPressed: (){
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: const Text("Are you sure you want to clear the cart?"),
+                        actions: [
+                          // cancel button
+                          TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text("Cancel"),
+                          ),
+
+                          // yes button
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                              restaurant.clearCart();
+                            },
+                            child: const Text("Yes"),
+                          ),
+                        ],
+                      ),
+                  );
+                },
+                icon: const Icon(Icons.delete),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+
+            // list of cart
+            Expanded(
+              child: Column(
+                children: [
+                  userCart.isEmpty
+                  ? const Expanded(
+                      child: Center(
+                        child: const Text("Cart is empty.."),
+                    ),
+                    )
+                  : Expanded(
+                      child: ListView.builder(
+                        itemCount: userCart.length,
+                          itemBuilder: (context, index) {
+                            // get individuals cart item
+                            final cartItem = userCart[index];
+
+                            // return cart tile UI
+                            return MyCartTile(cartItem: cartItem
+                            );
+                          },
+                      ),
+                  ),
+                ],
+              ),
+            ),
+
+            // button to pay
+            MyButton(
+                text: "Go to checkout",
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const PaymentPage(),
+                  ),
+                )
+            ),
+            const SizedBox(height: 25),
+          ],
+        ),
+      );
+    });
+  }
+}
